1b3721488787fef1aa96e121b3de6572d4de6063
lwmoreira <lwmoreira01@gmail.com>
2024-02-29 02:05:42 -0300

    iniciar a primeira migrations

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postres',
+  password: 'postgres',
+  database: 'codeburger',
+  define: {
+    timespamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+}
